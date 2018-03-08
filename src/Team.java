09d75e2ceef4706947508af3b75486df2806cec0
yudielgc <yguerra@salesforce.com>
2018-03-08 13:41:29 -0500

    initial commit

---
+++ src/Team.java
@@ -0,0 +1,15 @@
+package com.salesforce.hufflepuff;
+
+public class Team {
+
+	private String name;
+
+	public Team(String n) {
+		name = n;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
